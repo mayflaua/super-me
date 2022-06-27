68f763e8712a6d0d7b545f0f8bbbedb1736f4b42
sweetie <num1carbon@gmail.com>
2022-06-27 23:54:09 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <section class="content">
+    <div class="bg-clouds">Привет</div>
+  </section>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {};
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+.content {
+  width: 100%;
+  height: 100vh;
+  background: $dark-color;
+  color: white;
+  font-size: 64px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: "/",
+  filenameHashing: false,
+  productionSourceMap: false,
   transpileDependencies: true,
+
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/style/colors.scss";
+        `,
+      },
+    },
+  },
 });
